e436e6296c740f8c27f7942775dd6e67df2777ac
Railson Albino <railson@cbpf.br>
2026-05-09 12:55:15 -0300

    ⚡️ Criado footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="bg-zinc-900 text-gray-400 py-6 text-center border border-zinc-700">
+        <p>&copy; 2026 Railson Albino. Todos os direitos reservados.</p>
+    </footer>
+</template>
