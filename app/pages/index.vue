d7889e6f3d74c3111391562f0957afd7402b9263
Railson Albino <railson@cbpf.br>
2026-05-09 12:55:46 -0300

    🎨 adicionado sobre mim e algumas alterações de texto

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,37 +20,104 @@
 
       <div class="flex gap-4 flex-wrap justify-center">
         
+          <NuxtLink to="#projetos">
+            <button
+              class="bg-orange-700 hover:bg-orange-600 px-6 py-3 rounded-full font-semibold transition-all duration-300 hover:scale-105"
+            >
+              Ver Projetos
+            </button>
+          </NuxtLink>
+
+        <NuxtLink to="#sobremim">
           <button
-            class="bg-orange-700 hover:bg-orange-600 px-6 py-2 rounded-full font-semibold transition-all duration-300 hover:scale-105"
+            class="border border-zinc-700 hover:border-orange-500 px-6 py-3 rounded-full font-semibold text-gray-300 hover:text-white transition-all duration-300"
           >
-            Ver Projetos
+            Sobre mim
           </button>
-
-        <button
-          class="border border-zinc-700 hover:border-orange-500 px-6 py-3 rounded-full font-semibold text-gray-300 hover:text-white transition-all duration-300"
-        >
-          Sobre mim
-        </button>
-      </div>
-
-      <div class="mt-16 grid grid-cols-3 gap-8 text-center">
-        <div>
-          <h2 class="text-2xl font-bold text-orange-700">+3</h2>
-          <p class="text-gray-400 text-sm">Projetos</p>
-        </div>
-
-        <div>
-          <h2 class="text-2xl font-bold text-orange-700">+3</h2>
-          <p class="text-gray-400 text-sm">Anos estudando</p>
-        </div>
-
-        <div>
-          <h2 class="text-2xl font-bold text-orange-700">100%</h2>
-          <p class="text-gray-400 text-sm">Foco em Front-end</p>
-        </div>
+        </NuxtLink>
       </div>
     </div>
   </section>
+
+  <section
+  id="sobremim"
+  class="min-h-screen text-white px-6 flex items-center justify-center md:-mt-40 mb-40"
+>
+  <div
+    class="max-w-6xl w-full grid grid-cols-1 lg:grid-cols-2 gap-16 items-center"
+  >
+
+    <div class="relative flex justify-center">
+      <div
+        class="absolute w-72 h-72 bg-orange-700/20 blur-[10px] rounded-full"
+      />
+
+      <div
+        class="relative w-72 h-72 rounded-3xl overflow-hidden"
+      >
+        <img
+          src="/fotoperfil.png"
+          alt="Foto de perfil"
+          class="w-full h-full object-cover rounded-full"
+        />
+      </div>
+    </div>
+
+    <div>
+      <p class="text-orange-600 font-semibold tracking-[0.2em] uppercase mb-4">
+        Sobre mim
+      </p>
+
+      <h1 class="text-4xl md:text-5xl font-bold leading-tight mb-6">
+        Desenvolvedor Front-end focado em criar experiências modernas.
+      </h1>
+
+      <p class="text-gray-400 leading-relaxed mb-6">
+        Sou apaixonado por desenvolvimento web e atualmente foco na criação
+        de interfaces modernas, responsivas e interativas utilizando Vue,
+        Nuxt, TailwindCSS e TypeScript.
+      </p>
+
+      <p class="text-gray-400 leading-relaxed mb-8">
+        Gosto de transformar ideias em projetos visuais bem estruturados,
+        sempre buscando melhorar a experiência do usuário e evoluir minhas
+        habilidades como desenvolvedor.
+      </p>
+
+      <div class="grid grid-cols-2 gap-4 mb-8">
+
+        <div
+          class="border border-zinc-800 bg-zinc-900/60 rounded-2xl p-4"
+        >
+          <h2 class="text-3xl font-bold text-orange-600 mb-1">3+</h2>
+          <p class="text-gray-400 text-sm">Projetos desenvolvidos</p>
+        </div>
+
+        <div
+          class="border border-zinc-800 bg-zinc-900/60 rounded-2xl p-4"
+        >
+          <h2 class="text-3xl font-bold text-orange-600 mb-1">3+</h2>
+          <p class="text-gray-400 text-sm">Anos estudando</p>
+        </div>
+
+      </div>
+
+      <div class="flex flex-wrap gap-3 px-3">
+        <Icon name="devicon:vuejs" class="text-4xl text-green-500" />
+        <Icon name="devicon:nuxt" class="text-4xl text-green-500" />
+        <Icon name="devicon:tailwindcss" class="text-4xl text-green-500" />
+        <Icon name="devicon:typescript" class="text-4xl text-green-500" />
+        <Icon name="mdi:github" class="text-4xl text-white" />
+        <Icon name="devicon:git" class="text-4xl text-green-500" />
+        <Icon name="devicon:html5" class="text-4xl text-green-500" />
+        <Icon name="devicon:css3" class="text-4xl text-green-500" />
+        <Icon name="devicon:javascript" class="text-4xl text-green-500" />
+        <Icon name="devicon:mongodb" class="text-4xl text-green-500" />
+        <Icon name="devicon:postgresql" class="text-4xl text-green-500" />
+      </div>
+    </div>
+  </div>
+</section>
   
   <section
   id="projetos"
@@ -77,7 +144,7 @@
         </div>
 
         <h2 class="text-2xl font-bold mb-3">
-          Marcenaria Maranhão
+          Marcenaria<span class="text-orange-600"> Maranhão</span>
         </h2>
 
         <p class="text-gray-400 leading-relaxed mb-6">
@@ -116,7 +183,7 @@
         </div>
 
         <h2 class="text-2xl font-bold mb-3">
-          Teacher Nay
+          Teacher<span class="text-orange-600"> Nay</span>
         </h2>
 
         <p class="text-gray-400 leading-relaxed mb-6">
@@ -156,7 +223,7 @@
         </div>
 
         <h2 class="text-2xl font-bold mb-3">
-          Meu Portfólio
+          Meu<span class="text-orange-600"> Portfólio</span>
         </h2>
 
         <p class="text-gray-400 leading-relaxed mb-6">
@@ -164,13 +231,7 @@
           habilidades e experiências de forma moderna e responsiva.
         </p>
 
-        <div class="flex gap-3">
-          <NuxtLink
-            to="https://raideveloperportfolio.vercel.app/"
-            class="rounded-xl font-medium transition-all duration-300 hover:scale-105" target="_blank">
-            <Icon name="mdi:link" class="text-3xl text-white hover:text-orange-500 transition-all ease-out duration-300" />
-          </NuxtLink>
- 
+        <div class="flex gap-3"> 
           <NuxtLink
             to="https://github.com/railsonalb021/raideveloperportfolio"
             class="rounded-xl font-medium transition-all duration-300 hover:scale-105" target="_blank">
